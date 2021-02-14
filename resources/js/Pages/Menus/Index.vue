0aba5e62c1a445b35e3d6b54ba5ab2c89f88724b
Dharma Thapa <thapa.dharma017@gmail.com>
2021-02-15 00:10:16 +1100

    update

--- resources/js/Pages/Menus/Index.vue
+++ resources/js/Pages/Menus/Index.vue
@@ -116,6 +116,15 @@
           <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex">
             <div v-html="description" class="wrap-body"></div>
           </div>
+          <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+            <button
+              @click="closeModal()"
+              type="button"
+              class="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5"
+            >
+              Close
+            </button>
+          </span>
         </div>
       </div>
     </div>
@@ -181,6 +190,7 @@ export default {
   },
 };
 </script>
+
 <style scoped>
 .wrap-body {
   white-space: pre-wrap;
