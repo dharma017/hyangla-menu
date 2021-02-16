79725e352730df9247dab09c90350b32a03b18cd
Dharma Thapa <thapa.dharma017@gmail.com>
2021-02-16 22:12:54 +1100

    Fix frontend

--- resources/js/Pages/Home/Show.vue
+++ resources/js/Pages/Home/Show.vue
@@ -1,45 +1,106 @@
 <template>
   <div>
-    <menu-modal
-      :show="showMenuModal(menu.id)"
-      :image="menu.marketing_image"
-      @close="toggleMenuModal(menu.id)"
-    />
-    <organization-modal
-      :show="showOrganizationModal(organization.id)"
-      :image="organization.marketing_image"
-      @close="toggleOrganizationModal(organization.id)"
-    />
+    <div
+      class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400"
+      v-if="isOpen"
+    >
+      <div
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      >
+        <div class="fixed inset-0 transition-opacity">
+          <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span
+        >​
+        <div
+          class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg">
+            <button
+              aria-label="close"
+              class="absolute top-0 right-0 text-3xl text-gray-500 my-2 mx-4"
+              @click="closeModal()"
+            >
+              ×
+            </button>
+            <div class="modal-body">
+              <img
+                class="object-contain w-full"
+                slot="body"
+                v-if="menu.marketing_image"
+                :src="menu.marketing_image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div
+      class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400"
+      v-if="isOrgOpen"
+    >
+      <div
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      >
+        <div class="fixed inset-0 transition-opacity">
+          <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span
+        >​
+        <div
+          class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby="modal-headline"
+        >
+          <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg">
+            <button
+              aria-label="close"
+              class="absolute top-0 right-0 text-3xl text-gray-500 my-2 mx-4"
+              @click="closeOrganizationModal()"
+            >
+              ×
+            </button>
+            <div class="modal-body">
+              <img
+                class="object-contain w-full"
+                slot="body"
+                v-if="organization.marketing_image"
+                :src="organization.marketing_image"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <img class="object-contain w-full" v-if="menu.image" :src="menu.image" />
   </div>
 </template>
 
 <script>
-import MenuModal from "@/Pages/Menus/MarketingImageModal.vue";
-import OrganizationModal from "@/Pages/Organizations/MarketingImageModal.vue";
-
 export default {
   metaInfo: { title: "Hyangla Menu" },
-  components: {
-    MenuModal,
-    OrganizationModal,
-  },
   props: {
     menu: Object,
     organization: Object,
   },
   data() {
     return {
-      activeMenuModal: 0,
-      activeOrganizationModal: 0,
+      isOpen: false,
+      isOrgOpen: false,
     };
   },
   created() {
     if (this.organization.enable_marketing) {
-      this.toggleOrganizationModal(this.organization.id);
+      this.openOrganizationModal();
     } else {
       if (this.menu.enable_marketing) {
-        this.toggleMenuModal(this.menu.id);
+        this.openModal();
       }
     }
   },
@@ -47,25 +108,17 @@ export default {
     console.log("Component has been destroyed!");
   },
   methods: {
-    showMenuModal: function (id) {
-      return this.activeMenuModal === id;
+    openModal() {
+      this.isOpen = true;
     },
-    toggleMenuModal: function (id) {
-      if (this.activeMenuModal !== 0) {
-        this.activeMenuModal = 0;
-        return false;
-      }
-      this.activeMenuModal = id;
+    closeModal() {
+      this.isOpen = false;
     },
-    showOrganizationModal: function (id) {
-      return this.activeOrganizationModal === id;
+    openOrganizationModal() {
+      this.isOrgOpen = true;
     },
-    toggleOrganizationModal: function (id) {
-      if (this.activeOrganizationModal !== 0) {
-        this.activeOrganizationModal = 0;
-        return false;
-      }
-      this.activeOrganizationModal = id;
+    closeOrganizationModal() {
+      this.isOrgOpen = false;
     },
   },
 };
