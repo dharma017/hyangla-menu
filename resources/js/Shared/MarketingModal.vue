f846d3880be54cf075bc43926c8e9b0da5a420e7
Dharma Thapa <thapa.dharma017@gmail.com>
2021-02-20 15:44:41 +1100

    Fix padding

--- resources/js/Shared/MarketingModal.vue
+++ resources/js/Shared/MarketingModal.vue
@@ -2,7 +2,7 @@
   <Transition name="fade">
     <div
       v-if="showing"
-      class="fixed inset-0 w-full h-screen flex items-center justify-center bg-semi-75"
+      class="fixed inset-0 w-full h-screen flex items-center justify-center bg-semi-75 px-2"
       @click.self="close"
     >
       <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg">
