468a17fe4dee167c2fff6948b46b4d4d12549c4b
Dharma Thapa <thapa.dharma017@gmail.com>
2021-02-23 19:35:59 +1100

    Add google anayltics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,6 +12,17 @@
     <script src="https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith" defer></script>
 
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117992585-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-117992585-1');
+	</script>
+
     @routes
 </head>
 <body class="font-sans leading-none text-gray-700 antialiased">
